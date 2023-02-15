0c62b887bf8434ea4bdd67d860b569a53ce7599b
CamiloCursosSnowflake <125470248+CamiloCursosSnowflake@users.noreply.github.com>
2023-02-15 08:34:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
+
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3')
+streamlit.text('Kale')
+streamlit.text('HArd-Boiled')
+
+
